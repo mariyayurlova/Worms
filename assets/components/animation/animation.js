e7b8f1c44c1042f8a82f262fc4d9ce013ae90ad5
Маша Юрлова <mariya.yurlova@gmail.com>
2021-02-02 17:54:21 +0300

    Create home

--- assets/components/animation/animation.js
+++ assets/components/animation/animation.js
@@ -2,3 +2,21 @@ import './animation.scss';
 import $ from 'jquery';
 import Swiper from 'swiper';
 
+
+var swiper = new Swiper('.animation__container', {
+    breakpointsInverse: true,
+    slidesPerView: 1,
+    spaceBetween: 0,
+    breakpoints: {
+        768: {
+            spaceBetween: 20,
+            slidesPerView: 2},
+        1024: {
+            spaceBetween: 30,
+            slidesPerView: 3},
+        1280: {
+            slidesPerView: 4,
+            spaceBetween: 30
+        }
+    }
+});
--- assets/components/how/how.js
+++ assets/components/how/how.js
@@ -2,3 +2,20 @@ import './how.scss';
 import $ from 'jquery';
 import Swiper from 'swiper';
 
+var swiper = new Swiper('.how__container', {
+    breakpointsInverse: true,
+    slidesPerView: 1,
+    spaceBetween: 0,
+    breakpoints: {
+        768: {
+            spaceBetween: 20,
+            slidesPerView: 3},
+        1024: {
+            spaceBetween: 30,
+            slidesPerView: 4},
+        1280: {
+            slidesPerView: 6,
+            spaceBetween: 30
+        }
+    }
+});
